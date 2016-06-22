8933f43cd6b9401ea7fae5397adb0fce47557183
VTM-FAME <fame@Varjac.local>
2016-06-22 13:40:48 -0500

    Update to TLDR Archives
    Added Chou Chapters

--- tldrArchive/js/tldr.js
+++ tldrArchive/js/tldr.js
@@ -44,6 +44,10 @@ $(document).ready(function(){
 	articleItems.push(new Article("Arrowhead: Magicka","https://quip.com/4T9fAuWAeWx0",600,["games","development"],"games"));
 	articleItems.push(new Article("M Herman: Rules","https://quip.com/7PuuA7Yl7ufc",800,["games","development"],"games"));
 	articleItems.push(new Article("Ludwig Game Art","https://quip.com/QWw2AaVe90Kl",500,["games","design"],"games"));
+	articleItems.push(new Article("Mechanics Basics","https://quip.com/WaX1AvaGgz9K",800,["games","development"],"games"));
+	articleItems.push(new Article("Accomplishment","https://quip.com/QhhzAai1oAe8",800,["games","development"],"games"));
+	articleItems.push(new Article("Which Way to Win","https://quip.com/pg3cAKnl4OTb",800,["games","development"],"games"));
+	articleItems.push(new Article("Chou's Techniques","https://quip.com/PapOARw86ebe",800,["games","development"],"games"));
 	
 	//EDUCATION
 	articleItems.push(new Article("Good Question Game","https://quip.com/iUDRAFpLiDT5",500,["games","education"],"education"));
@@ -51,14 +55,18 @@ $(document).ready(function(){
 	//DESGIN
 	articleItems.push(new Article("Transparency","https://quip.com/qXCpAojvaNs4",700,["design"],"design"));
 	articleItems.push(new Article("Creative Spaces","https://quip.com/svxFApDVhrkw",500,["design"],"design"));
+	articleItems.push(new Article("Webfacing","https://quip.com/43rCAxkydhz9",500,["design"],"design"));
 	
 	//BUSINESS
-	articleItems.push(new Article("TLDR Archive","https://quip.com/QDiJAu9KsTa7",1000,["games","business"],"business"));
+	articleItems.push(new Article("TLDR Archive","https://quip.com/QDiJAu9KsTa7",1000,["business"],"business"));
 	articleItems.push(new Article("Job Description","https://quip.com/JN4LADIMxLuj",1000,["games","business"],"business"));
 	articleItems.push(new Article("Y Moon: Strengths","https://quip.com/E6m1AKQ4j93x",800,["business"],"business"));
+	articleItems.push(new Article("Team Composition","https://quip.com/iWE7AaCWOAVP",400,["business"],"business"));
+	articleItems.push(new Article("Early Access","https://quip.com/BFa9AyasmE7Z",600,["business", "games"],"business"));
 
 	//UXUI
 	articleItems.push(new Article("N Rosencranz: UXUI","https://quip.com/tuGHAxcuzvZB",500,["uxui"],"uxui"));
+	articleItems.push(new Article("Error Experience","https://quip.com/aq7uAWHGF0gZ",500,["uxui"],"uxui"));
 	
 	//PSYCHOLOGY
 	articleItems.push(new Article("Memory in Design","https://quip.com/TcNoArb2Ktkp",500,["uxui","psychology"],"psychology"));
